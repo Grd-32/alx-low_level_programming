2392d6f6fd8e7238bae3b96ee416e1c3581e4363
Grd-32 <pfdaniel77@gmail.com>
2022-04-08 23:56:46 -0700

    Working with random numbers in c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,27 +5,28 @@
 
 /* betty style doc for function main goes there */
 /**
- * A function to check whether a number is positive
+ * A function to find positive numbers
  * negative
  * or just a zero
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
+	if(n > 0)
 	{
-		printf ("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
-	else if (n == 0)
+	else if(n == 0)
 	{
-		printf ("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf ("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
